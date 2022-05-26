7f3b4db4aea534a7b7f8e35c4bda76291ce943d9
Yat Long Chan <ychan30@stuy.edu>
2022-05-26 16:06:54 -0400

    rules at top of board

--- v0/Board.java
+++ v0/Board.java
@@ -1,15 +1,23 @@
+/*
+BASIC SUDOKU RULES:
+
+1. Each row must contain 1-9 w/ no repetitions
+2. Each col must contain 1-9 w/ no repetitions
+3. Each 3 by 3 subsection must contain 1-9 w/ no repetitions
+*/
+
 public class Board {
     private int _board[][];
     private boolean _isModifiable[][];
 
-    // default constructor: 9*9 board
+    // default constructor: 9 by 9 board
     public Board() {
         _board = new int[9][9];
         _isModifiable = new boolean[9][9];
         //generate(randNum);
     }
 
-    // overload constructor: n*n board
+    // overload constructor: n by n board
     public Board(int n) {
         _board = new int[n][n];
         _isModifiable = new boolean[n][n];
@@ -28,10 +36,12 @@ public class Board {
         return oldVal;
     }
 
+    // fills sudoku board base on rules
     public void generate(int numIncluded) {
         /* TO BE IMPLEMENTED */
     }
 
+    // checks if puzzle is solved
     public boolean isSolved() {
         /* TO BE IMPLEMENTED */
         return false;
